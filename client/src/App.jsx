d972abcdf597efbceb35cdd1927ae170bd1aef3e
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-06-08 11:14:55 +0800

    Add basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <div className="text-red-500">This is the aasdaspp</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
