c0d0dff5d813e8d18f1b7d1058f6851961aac60d
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-06-08 12:10:13 +0800

    Add basic home page design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,8 +7,11 @@ const Home = () => {
       <Nav />
       <main>
         <section className="container mx-auto px-4">
-          <div className="flex justify-center">
+          <div className="flex justify-center items-center gap-8 flex-col mt-72">
             <h1 className="font-bold text-7xl">SWIPE RIGHT </h1>
+            <button className="py-4 px-6 bg-[#222222] rounded-full text-white text-center">
+              Create Account
+            </button>
           </div>
         </section>
       </main>
