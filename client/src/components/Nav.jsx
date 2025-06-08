d697f82fed056ba4f959f314f78cfd47eb66dca9
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-06-08 11:56:43 +0800

    Add basic nav

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="w-full">
+      <div className="container flex items-center justify-between mx-auto p-8">
+        <div>
+          <Link className="text-3xl font-bold" to="/">
+            Who's Next
+          </Link>
+        </div>
+        <div className="flex gap-2 items-center">
+          <ul className="flex gap-4 ">
+            <li>
+              <Link to="/">Products</Link>
+            </li>
+            <li>
+              <Link to="/">Learn</Link>
+            </li>
+            <li>
+              <Link to="/">Safety</Link>
+            </li>
+            <li>
+              <Link to="/">Support </Link>
+            </li>
+            <li>
+              <Link to="/">Download</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-8">
+          <button className="text-sm">Language</button>
+          <button className="bg-[#222222] py-2 px-8 rounded-full text-sm text-white w-full">
+            Login
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Nav from "../components/Nav";
 
 const Home = () => {
   return (
-    <div>
-      <p>This is the homepage</p>
-    </div>
+    <>
+      <Nav />
+      <main>
+        <section className="container mx-auto px-4">
+          <div className="flex justify-center">
+            <h1 className="font-bold text-7xl">SWIPE RIGHT </h1>
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
