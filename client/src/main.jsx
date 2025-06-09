cf2f20c15a11f8545f07877b2b51983117ca84fd
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-06-10 01:22:38 +0800

    Add a user auth logic and swipe view component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import "./styles/index.css";
 import "./styles/App.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
